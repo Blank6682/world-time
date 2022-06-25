8b40fd21e5c3e42e5fd05972c2c2622ffba9eb45
XiQuan Wu <2452402628@qq.com>
2022-06-26 07:22:00 +0800

    chore:init

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    <HelloWorld name="BlankZro" />
+  </div>
+</template>
+
+<style>
+html {
+  margin: 0;
+  padding: 0;
+  color-scheme: dark;
+  background: #222;
+  box-sizing: content-box;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const props = defineProps<{
+  name: string
+}>()
+
+const uppercase = computed(() => props.name.toUpperCase())
+</script>
+
+<template>
+  <div>
+    <h1>Hello,{{ uppercase }}</h1>
+  </div>
+</template>
+
+<style></style>
